c57000e2b53976802d97e787216a2b6e9f4bf5e9
Matteo Abis <gmatteo..abis@gmail.com>
2017-11-10 12:01:25 +0100

    all data

--- source/javascripts/vendor/sankey.js
+++ source/javascripts/vendor/sankey.js
@@ -91,7 +91,7 @@ d3.sankey = function() {
         cycleLaneDistFromFwdPaths = -10,  // the distance above the paths to start showing 'cycle lanes'
         cycleDistFromNode = 30,      // linear path distance before arcing from node
         cycleControlPointDist = 10,  // controls the significance of the cycle's arc
-        cycleSmallWidthBuffer = 2  // distance between 'cycle lanes'
+        cycleSmallWidthBuffer = 0  // distance between 'cycle lanes'
     ;
 
     function defaultId(d) {
@@ -210,7 +210,7 @@ d3.sankey = function() {
                     se_x = s_x + cycleDistFromNode,
                     se_y = s_y,
                     ne_x = se_x,
-                    ne_y = cycleLaneDistFromFwdPaths - (d.cycleIndex * (smallWidth + cycleSmallWidthBuffer) ),  // above regular paths, in it's own 'cycle lane', with a buffer around it
+                    ne_y = cycleLaneDistFromFwdPaths,  // above regular paths, in it's own 'cycle lane', with a buffer around it
                     nw_x = t_x - cycleDistFromNode,
                     nw_y = ne_y,
                     sw_x = nw_x,
